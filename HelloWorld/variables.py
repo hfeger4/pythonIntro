3f47b8ecd684da30b93a26e2424027ce122d8447
hfeger4 <hfeger4@gmail.com>
2017-10-12 20:18:34 -0700

    testing strings

--- HelloWorld/variables.py
+++ HelloWorld/variables.py
@@ -8,3 +8,13 @@ age = 28
 
 #Cant combine int and string....
 #print(age + greeting)
+
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
