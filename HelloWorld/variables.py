de043fca845ca1108c81311b776e121dea936fb9
hfeger4 <hfeger4@gmail.com>
2017-10-12 19:13:20 -0700

    testing variables

---
+++ HelloWorld/variables.py
@@ -0,0 +1,10 @@
+greeting = "Hello"
+_myName = "Howie"
+Howie34 = "Good"
+Time_Was_23 = "Hello"
+Greeting = "hello"
+print(Howie34 + " " + _myName)
+age = 28
+
+#Cant combine int and string....
+#print(age + greeting)
