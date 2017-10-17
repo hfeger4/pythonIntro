e8c1a399ef81b3a53c2be2ddfe7bb8a3781327c6
hfeger4 <hfeger4@gmail.com>
2017-10-16 21:36:43 -0700

    practice replacement fields and modifiers

--- HelloWorld/stringFormatting.py
+++ HelloWorld/stringFormatting.py
@@ -18,3 +18,22 @@ September: {1}
 October: {2}
 November: {1}
 December: {2}""".format(28,30,31))
+
+print("My age is %d years" % age)
+print("My age is %d %s, %d %s" % (age, "years", 6, "months"))
+
+for i in range(1,12):
+    print("No. %2d squared is %4d and cubed is %4d" %(i, i ** 2, i**3))
+
+print("PI is approximately %12.50f" % (22 / 7))
+
+#These % are mostly for python 2, not generally used in python 3
+
+for i in range(1,12):
+    print("No. {0:2} squared is {1:<4} and cubed is {2:<4}".format(i, i ** 2, i**3))
+
+#0 is the replacement number, 2 is the width
+#< left justifys it.
+
+print("PI is approximately {0:12.50}".format(22 / 7))
+
