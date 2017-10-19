23915966e74c73f2bdfb702c6e9b5244f410771c
hfeger4 <hfeger4@gmail.com>
2017-10-18 23:27:28 -0700

    first challenge complete

---
+++ HelloWorld/challenge1.py
@@ -0,0 +1,15 @@
+#Write a small program to ask for a name and an age.
+#When both values have been entered, check if the person
+#is the right age to go on an 18-30 holiday(they must be
+#over 18 and under 31)
+#If they are, welcome them to the holiday, otherwise print
+#a (polite) message refusing them entry.
+
+age = int(input("Please enter your age to continue: "))
+
+if 18 <= age <= 30:
+    print("Welcome to your holiday!")
+elif age < 18:
+    print("I'm sorry, you are too young to enjoy this holiday.")
+else:
+    print("I'm sorry, you are too old to enjoy this holdiay.")
