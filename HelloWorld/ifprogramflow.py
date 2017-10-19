3d923d05501cb25ca15f6af0e4341c53c95e712e
hfeger4 <hfeger4@gmail.com>
2017-10-18 23:20:58 -0700

    finish if and else tutorial

--- HelloWorld/ifprogramflow.py
+++ HelloWorld/ifprogramflow.py
@@ -40,11 +40,32 @@
 # else:
 #     print("Have a good day at work.")
 
-x= "false"
-if x:
-    print("x is true")
+# x= "false"
+# if x:
+#     print("x is true")
 
+# x = input("Please enter some text")
+# if x:
+#     print("You entered '{}'".format(x))
+# else:
+#     print("You did not enter anything")
+#
+#
 
+# age = int(input("How old are you? "))
+# if not(age < 18):
+#     print("You are old enough to vote")
+#     print("Put an 'X' in the box")
+# else:
+#     print("Please come back in {} years".format(18-age))
 
+# parrot = "Norwegian Blue"
+# 
+# letter = input("Enter a character: ")
+#
+# if letter in parrot:
+#     print("Give me an {}, Bob".format(letter))
+# else:
+#     print("I don't need that letter.")
 
 
