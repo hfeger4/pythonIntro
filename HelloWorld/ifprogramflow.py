97b2d43a152d27ca111f3d6e05959d073ab7fb93
hfeger4 <hfeger4@gmail.com>
2017-10-18 23:12:20 -0700

    more advance if and else added

--- HelloWorld/ifprogramflow.py
+++ HelloWorld/ifprogramflow.py
@@ -11,20 +11,40 @@
 #
 
 
-print("Please guess a number between 1-10: ")
-guess = int(input())
-if guess < 5:
-    print("Please guess higher")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it.")
-    else:
-        print("Sorry, you have not guessed correctly.")
-elif guess > 5:
-    print("Please guess lower")
-    if guess == 5:
-        print("Well done you guessed it.")
-    else:
-        print("Sorry you have not guessed correctly.")
-else:
-    print("You got it the first time!")
+# print("Please guess a number between 1-10: ")
+# guess = int(input())
+# if guess < 5:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it.")
+#     else:
+#         print("Sorry, you have not guessed correctly.")
+# elif guess > 5:
+#     print("Please guess lower")
+#     if guess == 5:
+#         print("Well done you guessed it.")
+#     else:
+#         print("Sorry you have not guessed correctly.")
+# else:
+#     print("You got it the first time!")
+
+# age = int(input("How old are you? "))
+
+# # if age >= 16 and age <= 65:
+# if 15 < age < 66: #same thing as above.
+#     print("Have a good day at work.")
+
+# if (age < 16) or (age > 65):
+#     print("Enjoy your free time.")
+# else:
+#     print("Have a good day at work.")
+
+x= "false"
+if x:
+    print("x is true")
+
+
+
+
+
