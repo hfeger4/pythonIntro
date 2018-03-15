d624f4d1b7552d779a990e72a1360e7bd96ecf88
hfeger4 <hfeger4@gmail.com>
2018-03-14 19:46:44 -0700

    change folder name to PythonReview

---
+++ PythonReview/sets.py
@@ -0,0 +1,11 @@
+farm_animals = {"sheep","cow","hen"}
+print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+
+wild_animals = {"lion","tiger","panther"}
+print(wild_animals)
+
+for animal in wild_animals:
+    print(animal)
