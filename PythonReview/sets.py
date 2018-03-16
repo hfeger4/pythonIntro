6b08826a8607057cb712f00df7d32c749f7dff34
hfeger4 <hfeger4@gmail.com>
2018-03-15 21:52:28 -0700

    add to sets

--- PythonReview/sets.py
+++ PythonReview/sets.py
@@ -4,8 +4,14 @@ print(farm_animals)
 for animal in farm_animals:
     print(animal)
 
-wild_animals = {"lion","tiger","panther"}
+wild_animals = set(["lion","tiger","panther"])
 print(wild_animals)
 
 for animal in wild_animals:
     print(animal)
+
+farm_animals.add("rooster")
+wild_animals.add("Dzung")
+
+print(farm_animals)
+print(wild_animals)
