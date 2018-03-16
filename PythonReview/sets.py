c4f917ebe6a34988145ebfca7e4111a5b0e39ce5
hfeger4 <hfeger4@gmail.com>
2018-03-15 22:25:13 -0700

    add to sets

--- PythonReview/sets.py
+++ PythonReview/sets.py
@@ -15,3 +15,35 @@ wild_animals.add("Dzung")
 
 print(farm_animals)
 print(wild_animals)
+
+empty_set = set()
+empty_set2 = {}
+empty_set.add("hello")
+print(empty_set)
+# empty_set2.add("no") Cant add if you initiate a set as empty open brackets, needs a value
+print(empty_set2)
+
+even = set(range(0,40,2))
+print(even)
+squares_tuple = (4,6,9,16,25)
+squares = set(squares_tuple)
+print(squares)
+even = set(range(0,40,2))
+print(even)
+print(len(even))
+
+print(even.union(squares))
+print(len(even.union(squares)))
+
+print(squares.union(even))
+
+print("-" * 40)
+
+print(even.intersection(squares))
+print(even & squares)
+print(squares.intersection(even))
+print(squares & even)
+
+print(sorted(even))
+
+
