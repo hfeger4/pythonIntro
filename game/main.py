f16c7eabcb4e7af8eab11262860250e3bfef18e6
hfeger4 <hfeger4@gmail.com>
2018-03-17 18:10:33 -0700

    start getters and setters

---
+++ game/main.py
@@ -0,0 +1,9 @@
+from player import Player
+
+tim = Player("Tim")
+
+print(tim.name)
+print(tim.lives)
+
+tim.lives -= 1
+print(tim.lives)
---
+++ oop/song.py
@@ -0,0 +1,15 @@
+class Song:
+    """Class to represent a song
+
+    Attributes:
+        title (str): The title of the song
+        artist (Artist): An artist object representing the songs creator.
+        duration (int): The duration of the song in seconds. May be zero
+        """
+
+    def __init__(self, title, artist, duration=0):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+
+    
