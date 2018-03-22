54583011527c4bd194f5fd7e04ff0ad1ad21591e
hfeger4 <hfeger4@gmail.com>
2018-03-22 15:16:58 -0700

    add hash version of anagram

--- elements_of_programming_interviews/anagram.py
+++ elements_of_programming_interviews/anagram.py
@@ -1,8 +1,30 @@
 def anagram(s1,s2):
-    s1 = s1.replace(" ", '').lower()
-    s2 = s2.replace(" ", '').lower()
+    s1 = s1.replace(" ","").lower()
+    s2 = s2.replace(" ", "").lower()
 
-    return sorted(s1) == sorted(s2)
+    if len(s1) != len(s2):
+        return False
 
-print(anagram("god", "dog"))
-print(anagram("go od", "d oo g"))
+    hash = {}
+
+    for letter in s1:
+        if letter in hash:
+            hash[letter] += 1
+        else:
+            hash[letter] = 1
+
+    for letter in s2:
+        if letter in hash:
+            hash[letter] -= 1
+        else:
+            hash[letter] = 1
+
+    for count in hash:
+        if hash[count] != 0:
+            return False
+
+    return True
+
+print(anagram("god", "dog") == True)
+print(anagram("go od", "d OO g") == True)
+print(anagram("oh","ooh") == False)
