e2b3db42d8bfd2997d40bf3cec38c785009c3239
hfeger4 <hfeger4@gmail.com>
2018-03-22 15:48:27 -0700

    add pair_sum

---
+++ elements_of_programming_interviews/pair_sum.py
@@ -0,0 +1,19 @@
+def pair_sum(arr, k):
+
+    if len(arr) < 2:
+        return False
+
+    seen = set()
+    output = set()
+
+    for num in arr:
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add( (min(num,target), max(num, target)) )
+
+    print(list(output))
+
+pair_sum([1,3,2,2],4)
