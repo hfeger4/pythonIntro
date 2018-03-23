635add792bb2b4c63c8b1068bb9ab8d232f539d9
hfeger4 <hfeger4@gmail.com>
2018-03-22 19:01:57 -0700

    add largest continious subsum

---
+++ elements_of_programming_interviews/largest_continous_sum.py
@@ -0,0 +1,22 @@
+def largest_continuous_sum(arr):
+    if len(arr) == 0:
+        return 0
+
+    max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(current_sum + num, num)
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
+
+# def large_cont_sum(arr):
+#     count = 0
+#     largest = arr[0]
+#     for num in arr:
+#         count += num
+#         if count > largest:
+#             largest = count
+#         if count < 0:
+#             count = 0
+#     return largest
