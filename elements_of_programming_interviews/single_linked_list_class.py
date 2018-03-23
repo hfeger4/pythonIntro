a8b652ec0439d22239e4a43ce7ccda135809ac68
hfeger4 <hfeger4@gmail.com>
2018-03-23 14:27:02 -0700

    add singly linked list class

---
+++ elements_of_programming_interviews/single_linked_list_class.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.nextnode = None
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+a.nextnode = b
+b.nextnode = c
+
+print(a.nextnode.value)
+print(a.value)
+
+
