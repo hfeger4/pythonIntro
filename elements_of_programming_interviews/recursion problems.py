0961959b19d5d5d8f0f572b5db482e59ac5b0c8a
hfeger4 <hfeger4@gmail.com>
2018-03-26 12:27:25 -0700

    add fib recursion and dynamic programming

--- elements_of_programming_interviews/recursion problems.py
+++ elements_of_programming_interviews/recursion problems.py
@@ -25,8 +25,6 @@ def word_split(phrase, list_of_words, output = None):
     return output
 
 
-
-
 print(word_split('themanran',['the','ran','man']))
 print(word_split('ilovedogsJohn',['i','am','a','dogs','lover','love','John']))
 print(word_split('themanran',['clown','ran','man']))
@@ -38,5 +36,65 @@ def reverse_string(str):
         return str[-1] + reverse_string(str[:-1])
 
 print(reverse_string("hello"))
+
+
+def permute(s):
+    out = []
+    if len(s) == 1:
+        out = [s]
+
+    else:
+        for i, let in enumerate(s):
+            for perm in permute(s[:i] + s[i + 1:]):
+                out += [let + perm]
+
+    return out
+
+print(permute('abc'))
+
+def fib_recursive(num):
+    if num == 0 or num == 1:
+        return num
+    return fib_recursive(num-1) + fib_recursive(num-2)
+
+print(fib_recursive(10))
+
+n = 12
+cache = [None] * (n + 1)
+
+def fib_dynamic(n):
+    if n == 0 or n == 1:
+        return n
+    if cache[n] != None:
+        return cache[n]
+    cache[n] = fib_dynamic(n-1) + fib_dynamic(n-2)
+
+    return cache[n]
+
+def fib_iterative_his_version(n):
+    a = 0
+    b = 1
+
+    for i in range(n):
+        a, b = b, a+b
+
+    return a
+
+
+def fib_iterative(n):
+    arr = [0,1]
+    position  = 2
+    if n == 0 or n == 1:
+        return n
+
+    while position <= n:
+        arr.append(arr[-2] + arr[-1])
+        position += 1
+
+    return arr[-1]
+
+print(fib_iterative(10))
+
+
 
 
