15040a712da0910f54aadc947c4e72fd35e60af0
hfeger4 <hfeger4@gmail.com>
2018-04-05 23:03:08 -0700

    change unique to interview question material

--- elements_of_programming_interviews/unique.py
+++ elements_of_programming_interviews/unique.py
@@ -1,29 +1,29 @@
+# def uni_char(s):
+#     return len(s) == len(set(s))
+
 def uni_char(s):
-    return len(s) == len(set(s))
+    hash = {}
+    for letter in s:
+        if letter in hash:
+            hash[letter] += 1
+        else:
+            hash[letter] = 1
 
-# def uni_char(s):
-#     hash = {}
-#     for letter in s:
-#         if letter in hash:
-#             hash[letter] += 1
-#         else:
-#             hash[letter] = 1
-#
-#     for count in hash:
-#         if hash[count] > 1:
-#             return False
-#
-#     return True
+    for count in hash:
+        if hash[count] > 1:
+            return False
 
-# def uni_char(s):
-#     my_set = set()
-#
-#     for char in s:
-#         if char in my_set:
-#             return False
-#         else:
-#             my_set.add(char)
-#     return True
+    return True
+
+def uni_char(s):
+    my_set = set()
+
+    for char in s:
+        if char in my_set:
+            return False
+        else:
+            my_set.add(char)
+    return True
 
 print(uni_char('abcdefg') == True)
 print(uni_char('abcdefgg') == False)
