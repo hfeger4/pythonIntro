c2b98bf2a5608bdb008af70cb82d61d65041525a
Howard Feger <hfeger4@gmail.com>
2018-05-12 15:30:28 -0700

    add graphs

--- PythonReview/graph_construct.py
+++ PythonReview/graph_construct.py
@@ -17,3 +17,43 @@ class Vertex:
 
     def __str__(self):
         return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self,key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self,n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def addEdge(self,f,t,cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+
+    def __contains__(self,n):
+        return n in self.vertList
+
+g = Graph()
+for i in range(6):
+    g.addVertex(i)
+
+print(g.vertList)
