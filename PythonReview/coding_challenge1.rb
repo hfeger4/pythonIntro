f26d1f6512fbbeac7805655adfa9c4d6e71493d4
Howard Feger <hfeger4@gmail.com>
2018-05-15 22:19:55 -0700

    add calc overlap

--- PythonReview/coding_challenge1.rb
+++ PythonReview/coding_challenge1.rb
@@ -27,3 +27,22 @@ def mult_except_current(arr)
 end
 
 p mult_except_current([1,2,3,4])
+
+def calc_overlap(coor1,coor2,dim1,dim2)
+  greater = [coor1,coor2].max
+  lower = [coor1+dim1,coor2+dim2].min
+
+  if greater >= lower
+    return [None,None]
+  end
+
+  overlap = lower - greater
+  return [greater,overlap]
+end
+
+def calc_rect_overlap(r1,r2)
+  x_overlap, w_overlap = calc_overlap(r1['x'],r1['w'],r2['x'],r2['w'])
+  y_overlap, h_overlap = calc_overlap(r1['y'],r1['h'],r2['y'],r2['h'])
+
+  return {'x': x_overlap, 'y': y_overlap,'w': w_overlap,'h': h_overlap}
+end
