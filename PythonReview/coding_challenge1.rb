e4ebc5f84eb84e737033b0749324f48efa54ce81
Howard Feger <hfeger4@gmail.com>
2018-05-15 22:11:46 -0700

    add second mock interview question

--- PythonReview/coding_challenge1.rb
+++ PythonReview/coding_challenge1.rb
@@ -12,3 +12,18 @@ def stock_price_profit_max(arr)
 end
 
 p stock_price_profit_max([10,12,14,12,13,11,8,7,6,13,23,45,11,10])
+
+def mult_except_current(arr)
+  ans_arr = []
+  i = 0
+  while i < arr.length
+    temp = arr[i]
+    arr[i] = 1
+    ans_arr << arr.reduce(:*)
+    arr[i] = temp
+    i += 1
+  end
+  ans_arr
+end
+
+p mult_except_current([1,2,3,4])
