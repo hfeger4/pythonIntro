029f00787997dfd37f69c83c7721a4ba05b4558b
Howard Feger <hfeger4@gmail.com>
2018-05-17 21:36:26 -0700

    add three integer product

--- PythonReview/coding_challenge3.rb
+++ PythonReview/coding_challenge3.rb
@@ -1,2 +1,9 @@
-def 3_integers(arr)
-end 
+def three_integers(arr)
+  arr = arr.sort
+  first = arr[0] * arr[1] * arr[-1]
+  second = arr[-3] * arr[-2] * arr[-1]
+  return [first,second].max
+end
+
+p three_integers([1,2,3,4,5,6,2,3,4,22,3])
+p three_integers([1,2,3,4,5,6,2,3,4,-22,3])
