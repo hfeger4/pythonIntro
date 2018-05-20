91979ac9218c52eb4c9d955db9f72327a6ecade1
Howard Feger <hfeger4@gmail.com>
2018-05-19 17:52:06 -0700

    add merge sort

--- PythonReview/coding_challenge3.rb
+++ PythonReview/coding_challenge3.rb
@@ -8,7 +8,19 @@ end
 p three_integers([1,2,3,4,5,6,2,3,4,22,3])
 p three_integers([1,2,3,4,5,6,2,3,4,-22,3])
 
-#time complexity O(nlogn) logn due to the constant division and n times to recreate the stack. 
-def merge_sort
+#time complexity O(nlogn) logn due to the constant division and n times to recreate the stack.
+def merge_sort(arr)
+  return arr if arr.length == 1
 
+  mid = arr.length/2
+  left = arr.drop(mid)
+  right = arr.take(mid)
+
+  sorted_left = merge_sort(left)
+  sorted_right = merge_sort(right)
+  return merge(sorted_left,sorted_right)
+end
+
+def merge(left,right)
+  arr = []
 end
